b36be6cc9e88eacb4ed269adaa6fb672a626342e
Rowan Seymour <rowanseymour@gmail.com>
2012-01-19 17:29:24 +0000

    Fixed localization on message validator

--- src/org/openmrs/module/sdmxhdintegration/SDMXHDMessageValidator.java
+++ src/org/openmrs/module/sdmxhdintegration/SDMXHDMessageValidator.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 
 package org.openmrs.module.sdmxhdintegration;
 
@@ -5,9 +18,8 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-
 /**
- * @author Jembi
+ * Validator for message objects
  */
 public class SDMXHDMessageValidator implements Validator {
 	
@@ -24,8 +36,7 @@ public class SDMXHDMessageValidator implements Validator {
 	 */
 	@Override
 	public void validate(Object obj, Errors e) {
-		ValidationUtils.rejectIfEmpty(e, "name", "field.required", "This field is required");
-		ValidationUtils.rejectIfEmpty(e, "sdmxhdZipFileName", "field.required.sdmxhdMessage.name", "A SDMX-HD Message must be uploaded");
-	}
-	
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "name", Constants.MODULE_ID + ".error.requiredField");
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "sdmxhdZipFileName", Constants.MODULE_ID + ".error.requiredFile");
+	}	
 }
